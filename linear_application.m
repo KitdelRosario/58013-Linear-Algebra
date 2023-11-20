4049a6a48950e2ae6b72a18b21212e7bae928308
KitdelRosario <124062219+KitdelRosario@users.noreply.github.com>
2023-11-20 09:20:14 +0800

    Add files via upload

---
+++ linear_application.m
@@ -0,0 +1,16 @@
+syms x y;
+eq1 = 20*x + 10*y == 350;
+eq2 = 17*x + 22*y == 500;
+[eq1,eq2]=equationsToMatrix([eq1,eq2],[x,y]);
+eq1;
+eq2;
+c=linsolve(eq1,eq2);
+c;
+plot(eq1,eq2);
+title('Linear Application');
+grid on;
+xlabel('total sales');
+ylabel('# of fruits');
+plot(eq1,eq2,'*');
+legend('Data points');
+plot(eq1,eq2);
